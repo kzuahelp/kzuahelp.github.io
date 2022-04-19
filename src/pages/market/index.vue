917272ba40cb93975be7a1018dd4d842f36663e6
kzuahelp <kzuahelp@protonmail.com>
2022-04-19 11:42:46 +0600

    Add market description

--- src/pages/market/index.vue
+++ src/pages/market/index.vue
@@ -24,6 +24,17 @@ loadItems();
 
 <template>
   <main :class="[$style.main, $style.limiter]">
+    <section :class="$style.section">
+      <h2 :class="$style.title" :id="id">{{ t("navigation.market") }}</h2>
+      <div :class="$style.panel">
+        <p>
+          Наша благотворительная ярмарка в онлайн-формате. Вся прибыль с продажи
+          представленных здесь товаров будет отправлена в качестве гуманитарной помощи
+          народу Украины.
+        </p>
+      </div>
+    </section>
+
     <div :class="[$style.group, $style.section]">
       <MarketCard
         v-for="item in items"
@@ -38,6 +49,9 @@ loadItems();
 </template>
 
 <style module lang="postcss">
+.description {
+}
+
 .section {
   margin: 0 40px 40px;
 }
@@ -72,5 +86,55 @@ loadItems();
     grid-template-columns: 1fr;
     gap: 20px;
   }
+}
+
+.section {
+  margin: 0 40px 40px;
+}
+
+@media only screen and (max-width: 1024px) {
+  .section {
+    margin: 20px;
+  }
+}
+
+.title {
+  font-size: 2.5rem;
+  line-height: 1.2;
+  font-weight: 500;
+  color: rgb(var(--accent-color));
+  display: flex;
+  align-items: center;
+  margin: 0;
+  padding: 40px 0;
+
+  &::before {
+    content: "";
+    display: block;
+    width: 100px;
+    height: 28px;
+    background: rgb(var(--accent-color));
+    margin-right: 20px;
+    border-radius: 20px;
+  }
+}
+
+@media only screen and (max-width: 1024px) {
+  .title {
+    font-size: 1.8rem;
+    margin: 0 0 20px;
+    padding: 20px 0;
+
+    &::before {
+      width: 60px;
+      height: 28px;
+    }
+  }
+}
+
+.panel {
+  font-size: 1.25rem;
+  display: flex;
+  flex-flow: column;
 }
 </style>
