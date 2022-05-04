9b7de2f1e2d1d747bda087d54c78bb2071db9543
kzuahelp <102131403+kzuahelp@users.noreply.github.com>
2022-05-04 06:33:07 +0000

    Deploying to gh-pages from @ kzuahelp/kzuahelp.github.io@cff82149922a901f05d45933f67c0d7b18258d89 🚀

---
+++ assets/_...all_.9408bea4.js
@@ -0,0 +1,1 @@
+import{g as _}from"./index.b5695b29.js";const r={};function e(n,t){return"Page not found"}var c=_(r,[["render",e]]);export{c as default};
---
+++ assets/item.en.fb1406e6.js
@@ -0,0 +1,1 @@
+import{b as t}from"./route-block.b5bad31b.js";import{o,c as r}from"./index.b5695b29.js";const a={class:"article"},n={setup(s,{expose:e}){return e({frontmatter:{title:"Coat of arms of Ukraine, Orange",description:"Pendant, side hole, 20 mm, handmade",author:"https://t.me/thegello",cost:"3000\u20B8"}}),(i,m)=>(o(),r("div",a))}};typeof t=="function"&&t(n);export{n as default};
