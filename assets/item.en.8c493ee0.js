2fd444c920ee3bb436c42f075d4bcaaf135a935e
kzuahelp <102131403+kzuahelp@users.noreply.github.com>
2022-05-04 06:44:25 +0000

    Deploying to gh-pages from @ kzuahelp/kzuahelp.github.io@e7e28c0d4270822a039f99255fd08afe3895aab3 🚀

---
+++ assets/item.en.8c493ee0.js
@@ -0,0 +1,1 @@
+import{b as t}from"./route-block.b5bad31b.js";import{o,c as r}from"./index.2c82e5b6.js";const a={class:"article"},c={setup(n,{expose:e}){return e({frontmatter:{title:"Heart, Red",description:"Badge or magnet. 32 mm, handmade",author:"volunteer",cost:"3000\u20B8"}}),(m,i)=>(o(),r("div",a))}};typeof t=="function"&&t(c);export{c as default};
