6bea530592584e730d78192353faf69e38f7da9d
kzuahelp <102131403+kzuahelp@users.noreply.github.com>
2022-05-26 10:50:20 +0000

    Deploying to gh-pages from @ kzuahelp/kzuahelp.github.io@dc2209b18b312487bfa05752e47119b2867d8ade 🚀

---
+++ assets/item.en.1c83d8df.js
@@ -0,0 +1,1 @@
+import{b as t}from"./route-block.b5bad31b.js";import{o,e as r}from"./main.3942ab43.js";const a={class:"article"},c={setup(n,{expose:e}){return e({frontmatter:{title:"Framed Child's Drawing",description:"Framed drawing, A4",author:"volunteer",cost:"5000\u20B8"}}),(i,l)=>(o(),r("div",a))}};typeof t=="function"&&t(c);export{c as default};
---
+++ assets/item.en.6a85638e.js
@@ -0,0 +1,1 @@
+import{b as t}from"./route-block.b5bad31b.js";import{o,e as a}from"./main.3942ab43.js";const r={class:"article"},c={setup(n,{expose:e}){return e({frontmatter:{title:"Peace sign, Blue, Black and White Background",description:"Badge. 32 mm, handmade",author:"volunteer",cost:"3000\u20B8"}}),(i,l)=>(o(),a("div",r))}};typeof t=="function"&&t(c);export{c as default};
