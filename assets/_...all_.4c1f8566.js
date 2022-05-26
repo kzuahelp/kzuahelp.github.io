55f4154842e58b569e6fa76a28bc2e070fb72b1b
kzuahelp <102131403+kzuahelp@users.noreply.github.com>
2022-05-26 10:08:47 +0000

    Deploying to gh-pages from @ kzuahelp/kzuahelp.github.io@efb12272f30eca369ee09bb85d2f8809e2105049 🚀

---
+++ assets/_...all_.4c1f8566.js
@@ -0,0 +1,1 @@
+import{j as _}from"./main.4eab1cc9.js";const r={};function e(n,t){return"Page not found"}var c=_(r,[["render",e]]);export{c as default};
---
+++ assets/item.en.c50f62fe.js
@@ -0,0 +1,1 @@
+import{b as t}from"./route-block.b5bad31b.js";import{o,e as a}from"./main.4eab1cc9.js";const r={class:"article"},n={setup(c,{expose:e}){return e({frontmatter:{title:"Peace sign, Red and White, Yellow and Blue background",description:"Badge. 32 mm, handmade",author:"volunteer",cost:"3000\u20B8"}}),(i,l)=>(o(),a("div",r))}};typeof t=="function"&&t(n);export{n as default};
