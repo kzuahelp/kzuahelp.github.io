3b0c24dcaf66ad98d1cc4981079e02649f3acdb9
kzuahelp <102131403+kzuahelp@users.noreply.github.com>
2022-07-04 16:46:33 +0000

    Deploying to gh-pages from @ kzuahelp/kzuahelp.github.io@06c33f8029dc77c12aa813eceeeb1f155e11aa62 🚀

--- assets/item.en.59c26e9f.js
+++
@@ -1,1 +0,0 @@
-import{b as t}from"./route-block.b5bad31b.js";import{o,e as r}from"./main.72e74292.js";const a={class:"article"},c={setup(n,{expose:e}){return e({frontmatter:{title:"Heart, Yellow and Blue background",description:"Badge. 32 mm, handmade",author:"volunteer",cost:"3000\u20B8"}}),(l,i)=>(o(),r("div",a))}};typeof t=="function"&&t(c);export{c as default};
--- assets/item.en.f17f909c.js
+++
@@ -1,1 +0,0 @@
-import{b as t}from"./route-block.b5bad31b.js";import{o as e,e as r}from"./main.72e74292.js";const c={class:"article"},a={setup(s,{expose:o}){return o({frontmatter:{title:"Handmade Postcard",description:"Postcard, \u04106",author:"volunteer",cost:"500\u20B8"}}),(i,f)=>(e(),r("div",c))}};typeof t=="function"&&t(a);export{a as default};
